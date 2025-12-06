13f3cd26260c65f2bc876a412e34ab23bb86108c
Sergio Palacios 2 <sergid26pal@gmail.com>
2025-12-06 18:11:55 -0500

    FEATURE. Implementacion de Docker en el proyecto usando docker compose

--- backend/main.py
+++ backend/main.py
@@ -11,12 +11,8 @@ app = FastAPI(lifespan=crear_tablas)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://127.0.0.1:5500",
-        "http://127.0.0.1:8000",
-        "http://192.168.0.21:5500",
-        "http://192.168.0.21:8000",
-        "http://10.0.211.231:5500",
-        "http://10.0.211.231:8000/"
+        "http://127.0.0.1:8080",
+        "http://localhost:8080",
     ],
     allow_credentials=True,
     allow_methods=["*"],
