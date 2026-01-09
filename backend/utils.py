3dd55e8f1ebc3b125af1e3ecbcd3440c9acb981d
Sergio Palacios 2 <sergid26pal@gmail.com>
2026-01-09 11:59:53 -0500

    FIX. Se usa un reverse proxy para acceder al backend y al minijuego, se prepara todo para usar HTTPs

--- backend/utils.py
+++ backend/utils.py
@@ -6,7 +6,7 @@ load_dotenv()
 DOMAIN:str = os.getenv("DOMINIO")
 
 DOMAIN_FR:str = f"{DOMAIN}"
-DOMAIN_BK:str = f"{DOMAIN}:8000"
+DOMAIN_BK:str = f"{DOMAIN}/api"
 
 IMG_PATH:str = "uploads"
 IMG_PATH_POSTS:str = "uploads/posts"
