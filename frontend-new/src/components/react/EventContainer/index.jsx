06ac640287083d1fc3851f332171db9c7c443061
Sergio Palacios 2 <sergid26pal@gmail.com>
2026-01-25 21:25:01 -0500

    FEATURE. Front-New. Creación de la página de Eventos, se prepara el componente de react para la interactividad

---
+++ frontend-new/src/components/react/EventContainer/index.jsx
@@ -0,0 +1,12 @@
+import { useState, useEffect } from 'react';
+import styles from './styles.module.css';
+
+const API_BASE = 'http://localhost:8000';
+
+function EventContainer() {
+  return (<>
+    Aqui va la logica
+  </>);
+}
+
+export { EventContainer };
