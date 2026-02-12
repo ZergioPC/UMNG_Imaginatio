c1445edb95a967a003c0ad762d269aefd3fd84a0
Sergio Palacios 2 <sergid26pal@gmail.com>
2026-02-11 22:19:58 -0500

    FEATURE. Se agregan los enlaces correspondientes al header y footer

--- frontend/src/components/react/Header/index.jsx
+++ frontend/src/components/react/Header/index.jsx
@@ -15,8 +15,7 @@ const navContent = [
   {txt:"Mascota", link:"/mascota"},
   {txt:"Quienes Somos", link:"/about"},
   {txt:"Concursos", link:"/concursos"},
-  {txt:"Eventos Pasados", link:"/eventos-pasados"},
-  {txt:"Edición 2026", link:"/2026"},
+  {txt:"Eventos Pasados", link:"/eventos-pasados"}
 ];
 
 function Header(){
