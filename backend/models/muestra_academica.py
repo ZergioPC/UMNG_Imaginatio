7199a8df45c42abca7553aeed924b5aa83849f3d
Sergio Palacios 2 <sergid26pal@gmail.com>
2026-04-07 14:13:27 -0500

    backend. Se agrega propiedad para enlaces externos en muesta_academica

--- backend/models/muestra_academica.py
+++ backend/models/muestra_academica.py
@@ -4,10 +4,12 @@ from typing import Optional
 class MuestraAcademicaBase(SQLModel):
     title:str
     img:str = "uploads/users/idle.jpg"
+    page:str = "#"
 
 class MuestraAcademicaEdit(SQLModel):
     title: Optional [str] = Field(default="")
-    img: Optional [str] = Field(default=None) 
+    img: Optional [str] = Field(default=None)
+    page: Optional [str] = Field(default="") 
 
 class MuestraAcademica(MuestraAcademicaBase, table=True):
     __tablename__ = "muestra_academica"
